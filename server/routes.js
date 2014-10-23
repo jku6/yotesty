7154c34103c3825ba058da0959e2986b3fdf9185
Jon <thejonku@gmail.com>
2014-10-22 20:43:30 -0400

    First

---
+++ server/routes.js
@@ -0,0 +1,33 @@
+/**
+ * Main application routes
+ */
+
+'use strict';
+
+var errors = require('./components/errors');
+
+module.exports = function(app) {
+
+
+  // app.get('/', function(req, res, next) {
+  //   // Handle the get for this route
+  // });
+
+  // app.post('/', function(req, res, next) {
+  //  // Handle the post for this route
+  // });
+
+  // Insert routes below
+  app.use('/api/things', require('./api/thing'));
+  
+  // All undefined asset or api routes should return a 404
+  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+   .get(errors[404]);
+
+  // All other routes should redirect to the index.html
+  app.route('/*')
+    .get(function(req, res) {
+      res.sendfile(app.get('appPath') + '/index.html');
+    });
+
+};
