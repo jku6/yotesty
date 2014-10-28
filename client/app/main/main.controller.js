e64ea685869e01805325b493f0d42e7666cac4f2
Jon <thejonku@gmail.com>
2014-10-27 20:49:14 -0400

    Add initial d3 with glassdoor

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,21 +2,21 @@
 
 angular.module('testyApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
+    var url = "http://api.glassdoor.com/api/api.htm?t.p=25924&t.k=fib3ZtVEqg3&userip=0.0.0.0&useragent=&format=json&v=1&action=jobs-stats&returnStates=true&admLevelRequested=1&jt=";
+    
+    $http.get(url+"analyst").
+    success(function(data, status, headers, config) {
+      var ret = data["response"]["states"];
+      var dataobj = [];
+      for (var aa in ret) {
+        var obj = ret[aa];
+        dataobj.push([obj["name"],obj["numJobs"]]);
       }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
+      console.log(dataobj);
+      $scope.jobData = dataobj;
+    }).
+    error(function(data, status, headers, config) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+    });
   });
